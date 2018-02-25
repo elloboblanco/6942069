55543ddef736124b05e5e13fa22ab247d62406e8
Will Foster <williamgfoster@gmail.com>
2018-02-24 23:06:06 -0700

    Add something dynamic

--- aws-java-maven/src/main/java/com/fosterizo/Handler.java
+++ aws-java-maven/src/main/java/com/fosterizo/Handler.java
@@ -1,13 +1,16 @@
 package com.fosterizo;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.Map;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Logger;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
@@ -16,9 +19,12 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
 		BasicConfigurator.configure();
-
 		LOG.info("received request");
-		Response responseBody = new Response("Fosterizo Rules!");
+
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		String boiseTime = dtf.format(LocalDateTime.now(ZoneId.of("America/Boise")));
+		Response responseBody = new Response("Fosterizo Rules! It is currently " + boiseTime);
+
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
 				.setObjectBody(responseBody)

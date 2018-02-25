bd35a6c9be668a98816dec245faf9f2ae0dfb768
Will Foster <williamgfoster@gmail.com>
2018-02-24 22:16:12 -0700

    v1 of fosterizo-api

---
+++ aws-java-maven/src/main/java/com/fosterizo/Handler.java
@@ -0,0 +1,28 @@
+package com.fosterizo;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+	private static final Logger LOG = Logger.getLogger(Handler.class);
+
+	@Override
+	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+		BasicConfigurator.configure();
+
+		LOG.info("received request");
+		Response responseBody = new Response("Fosterizo Rules!");
+		return ApiGatewayResponse.builder()
+				.setStatusCode(200)
+				.setObjectBody(responseBody)
+				.setHeaders(Collections.singletonMap("X-Powered-By", "Fosterizo Rules!"))
+				.build();
+	}
+}
